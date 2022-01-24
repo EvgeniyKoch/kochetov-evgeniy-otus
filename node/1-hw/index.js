105924cae5dac9cf90f08409737b2ab31ed28b31
ekochetov <ekochetov@roox.ru>
2022-01-24 15:55:32 +0300

    resolve first task

---
+++ node/1-hw/index.js
@@ -0,0 +1,51 @@
+const data = {
+  name: 1,
+  items: [
+    {
+      name: 2,
+      items: [
+        { name: 3 },
+        {
+          name: 4,
+          items: [
+            {
+              name: 7,
+              items: [{ name: 234 }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 5,
+      items: [{ name: 6, items: [{ name: 234 }] }],
+    },
+  ],
+};
+
+const ident = (depth, tab = 2) => " ".repeat(depth * tab);
+
+const dirParent = "├───";
+const dirChild = "└───";
+
+const showTree = ({ name, items }) => {
+  const iter = (list, isChild, str, count) => {
+    const space = isChild ? `│${ident(count)}` : "";
+    const dir = isChild ? dirChild : dirParent;
+
+    if (!list) {
+      return str;
+    }
+
+    return list.reduce((acc, item) => {
+      const line = `${acc}${space}${dir}${item.name}\n`;
+      const valueIdent = isChild ? count + 1 : 1;
+
+      return iter(item.items, true , line, valueIdent);
+    }, str);
+  };
+
+  return iter(items, false, `${name}\n`, 1);
+};
+
+console.log(showTree(data));
